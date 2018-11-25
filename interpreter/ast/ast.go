1c3324fe00c5f358b5157d2c10fee5055f248124
Björn Gylling <bjorn.gylling@gmail.com>
2018-11-25 12:09:28 +0100

    fix formatting

--- interpreter/ast/ast.go
+++ interpreter/ast/ast.go
@@ -19,6 +19,7 @@ type Expression interface {
 type Program struct {
 	Statement []Statement
 }
+
 func (p *Program) TokenLiteral() string {
 	if len(p.Statement) > 0 {
 		return p.Statement[0].TokenLiteral()
@@ -31,13 +32,15 @@ type Identifier struct {
 	Token token.Token
 	Value string
 }
-func (i *Identifier) expressionNode() {}
+
+func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 
 type LetStatement struct {
 	Token token.Token
-	Name *Identifier
+	Name  *Identifier
 	Value Expression
 }
-func (ls *LetStatement) statementNode() {}
+
+func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
